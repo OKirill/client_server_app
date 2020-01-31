283b8140627a5084706444bd0a043095c4cf8aa1
Kirill <hotasice41@gmail.com>
2020-01-31 14:43:54 +0100

    done server

--- third/server.py
+++ third/server.py
@@ -5,20 +5,21 @@ import socket
 import sys
 import json
 import logging
+import select
+import time
 import argparse
 import logs.config_server_log
 from errors import IncorrectDataReceivedError
 from backup.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-    PRESENCE, TIME, USER, ERROR, DEF_PORT, DEF_IP_ADRRES
+    PRESENCE, TIME, USER, ERROR, DEF_PORT, MESSAGE_TEXT, MESSAGE, SENDER
 from backup.utils import rec_message, transmit_message
 from decos import log
-
 
 LOGGER = logging.getLogger('server')
 
 
 @log
-def handling_mess_from_client(message):
+def handling_mess_from_client(message, messages_list, client):
     """
     Обработка сообщения от клиента в виде словаря,
     проверка и отправка ответа.
@@ -28,11 +29,18 @@ def handling_mess_from_client(message):
     LOGGER.debug(f'Обработка сообщения клиента: {message}')
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
-        return {RESPONSE: 200}
-    return {
-        RESPONSE: 400,
-        ERROR: 'False Request'
-    }
+        transmit_message(client, {RESPONSE: 200})
+        return
+    elif ACTION in message and message[ACTION] == MESSAGE and \
+            TIME in message and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+        return
+    else:
+        transmit_message(client, {
+            RESPONSE: 400,
+            ERROR: 'False Request'
+        })
+        return
 
 
 @log
@@ -44,7 +52,17 @@ def parser_handling():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', default=DEF_PORT, type=int, nargs='?')
     parser.add_argument('-a', default='', nargs='?')
-    return parser
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+
+    if not 1023 < listen_port < 65536:
+        LOGGER.critical(
+            f'Попытка запуска сервера с указанием неподходящего порта '
+            f'{listen_port}. Допустимы адреса с 1024 до 65535.')
+        sys.exit(1)
+
+    return listen_address, listen_port
 
 
 def main():
@@ -54,10 +72,16 @@ def main():
     параметры по умолчанию
     :return:
     """
-    parser = parser_handling()
-    namespace = parser.parse_args(sys.argv[1:])
-    listen_address = namespace.a
-    listen_port = namespace.p
+    listen_address, listen_port = parser_handling()
+    LOGGER.info(
+        f'Запущен сервер, порт для подключений: {listen_port}, '
+        f'адрес с которого принимаются подключения: {listen_address}. '
+        f'Если адрес не указан, принимаются соединения с любых адресов.')
+
+    # Готовим сокет
+    forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    forward.bind((listen_address, listen_port))
+    forward.settimeout(0.5)
 
     # try:
     #     if '-p' in sys.argv:
@@ -85,42 +109,96 @@ def main():
     #     print('Укажите адресс который будет прослушивать сервер после параметра -\'a\'.')
     #     sys.exit(1)
 
-    if not 1023 < listen_port < 65536:
-        LOGGER.critical(
-            f'Запуск сервера с неправильным портом: {listen_port}.'
-            f' Используйте адреса в диапазоне 1024-65535.'
-        )
-        sys.exit(1)
+    # if not 1023 < listen_port < 65536:
+    #     LOGGER.critical(
+    #         f'Запуск сервера с неправильным портом: {listen_port}.'
+    #         f' Используйте адреса в диапазоне 1024-65535.'
+    #     )
+    #     sys.exit(1)
+    #
+    # LOGGER.info(
+    #     f'Сервер запущен с портом для подключения: {listen_port}'
+    #     f'адрес сервера: {listen_address}')
+    #
+    # forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # forward.bind((listen_address, listen_port))
+    #
+    # forward.listen(MAX_CONNECTIONS)
+    #
+    # while True:
+    #     client, client_adress = forward.accept()
+    #     try:
+    #         message_from_client = rec_message(client)
+    #         LOGGER.debug(f'Получено сообщение {message_from_client}')
+    #         response = handling_mess_from_client(message_from_client)
+    #         LOGGER.info(f'Создан ответ для клиента {response}')
+    #         transmit_message(client, response)
+    #         LOGGER.debug(f'Сеанс с клиентом {client_adress} завершен.')
+    #         client.close()
+    #     except json.JSONDecodeError:
+    #         LOGGER.error(
+    #             f'Не удалось обработать Json  полученый от '
+    #             f'клиента {client_adress}. Сеанс завершен.'
+    #         )
+    #         client.close()
+    #     except IncorrectDataReceivedError:
+    #         LOGGER.error(f'От клиента {client_adress} приняты некорректные данные. '
+    #                             f'Соединение закрывается.')
+    #         client.close()
+    # список клиентов
+    clients = []
+    messages = []
 
-    LOGGER.info(
-        f'Сервер запущен с портом для подключения: {listen_port}'
-        f'адрес сервера: {listen_address}')
-
-    forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    forward.bind((listen_address, listen_port))
-
+    # Слушаем порт
     forward.listen(MAX_CONNECTIONS)
-
+    # Основной цикл программы сервера
     while True:
-        client, client_adress = forward.accept()
+        # Ждём подключения, если таймаут вышел, ловим исключение.
         try:
-            message_from_client = rec_message(client)
-            LOGGER.debug(f'Получено сообщение {message_from_client}')
-            response = handling_mess_from_client(message_from_client)
-            LOGGER.info(f'Создан ответ для клиента {response}')
-            transmit_message(client, response)
-            LOGGER.debug(f'Сеанс с клиентом {client_adress} завершен.')
-            client.close()
-        except json.JSONDecodeError:
-            LOGGER.error(
-                f'Не удалось обработать Json  полученый от '
-                f'клиента {client_adress}. Сеанс завершен.'
-            )
-            client.close()
-        except IncorrectDataReceivedError:
-            LOGGER.error(f'От клиента {client_adress} приняты некорректные данные. '
-                                f'Соединение закрывается.')
-            client.close()
+            client, client_address = forward.accept()
+        except OSError:
+            pass
+        else:
+            LOGGER.info(f'Установлено соедение с ПК {client_address}')
+            clients.append(client)
+
+        recv_data_lst = []
+        send_data_lst = []
+        err_lst = []
+        # Проверяем на наличие ждущих клиентов
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        # принимаем сообщения и если там есть сообщения,
+        # кладём в словарь, если ошибка, исключаем клиента.
+        if recv_data_lst:
+            for client_with_message in recv_data_lst:
+                try:
+                    handling_mess_from_client(rec_message(client_with_message),
+                                              messages, client_with_message)
+                except:
+                    LOGGER.info(f'Клиент {client_with_message.getpeername()} '
+                                f'отключился от сервера.')
+                    clients.remove(client_with_message)
+
+        # Если есть сообщения для отправки и ожидающие клиенты, отправляем им сообщение.
+        if messages and send_data_lst:
+            message = {
+                ACTION: MESSAGE,
+                SENDER: messages[0][0],
+                TIME: time.time(),
+                MESSAGE_TEXT: messages[0][1]
+            }
+            del messages[0]
+            for waiting_client in send_data_lst:
+                try:
+                    transmit_message(waiting_client, message)
+                except:
+                    LOGGER.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                    clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
